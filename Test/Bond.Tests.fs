62935700edad3ee1f6b09b1935d38131b33259f3
Tomas <tomas.sundvall@gmail.com>
2023-01-15 08:54:43 +0100

    Added a few more tests and cleaned up the test project a bit

--- Test/Bond.Tests.fs
+++ Test/Bond.Tests.fs
@@ -11,6 +11,8 @@ let ``My test`` () =
 [<Theory>]
 //           result           settl         mat            rate        yld            redemption    freq
 [<InlineData(88.18822,        "2023-01-14", "2029-04-20",   0.07294,    0.098415429,   100.,         4.)>]
+[<InlineData(90.13965,        "2023-01-15", "2024-01-01",   0.07294,    0.187362464,   100.,         4.)>]
+[<InlineData(89.78170,        "2023-01-01", "2024-01-01",   0.07294,    0.187362464,   100.,         4.)>]
 let ``Test valid inputs for Bond.price function for UsPsa30_360 basis`` (r, settl, mat, rate, yld, redemption, freq) =
   let toFiveDec (x : float) =
     Math.Round (x, 5)
@@ -33,7 +35,7 @@ let ``Test getPriceYieldFactors with UsPsa30_300 basis`` () =
   let basis = Excel.FinancialFunctions.DayCountBasis.UsPsa30_360
 
   let numOfCoupons, _, daysBetween, couponDays, dsc = getPriceYieldFactors settl mat freq basis
-  //Assert.Equal<float>(26, numOfCoupons)
-  //Assert.Equal<float>(84, daysBetween)
+  Assert.Equal<float>(26, numOfCoupons)
+  Assert.Equal<float>(84, daysBetween)
   Assert.Equal<float>(90, couponDays)
-  //Assert.Equal<float>(6, dsc)
+  Assert.Equal<float>(6, dsc)
