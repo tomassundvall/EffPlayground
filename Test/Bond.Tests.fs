ba569eeb3235d1464d3b7b121ec9e6cebf7691ec
Tomas <tomas.sundvall@gmail.com>
2023-01-16 21:12:27 +0100

    Added test for calculating yield

--- Test/Bond.Tests.fs
+++ Test/Bond.Tests.fs
@@ -4,9 +4,11 @@ open System
 open Xunit
 open Excel.FinancialFunctions.Bond
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let toFiveDec (x : float) =
+  Math.Round (x, 5)
+
+let toFourDec (x : float) =
+  Math.Round (x, 4)
 
 [<Theory>]
 //           result            settl         mat            rate        yld            redemption    freq
@@ -15,9 +17,6 @@ let ``My test`` () =
 [<InlineData(89.78170,        "2023-01-01", "2024-01-01",   0.07294,    0.187362464,   100.,         4.)>]
 [<InlineData(90.36331,        "2023-01-01", "2024-01-01",   0.07294,    0.187362464,   100.,         1.)>]
 let ``Test valid inputs for Bond.price function for UsPsa30_360 basis`` (r, settl, mat, rate, yld, redemption, freq) =
-  let toFiveDec (x : float) =
-    Math.Round (x, 5)
-
   let settl' = DateTime.Parse(settl)
   let mat' = DateTime.Parse(mat)
   
@@ -26,6 +25,20 @@ let ``Test valid inputs for Bond.price function for UsPsa30_360 basis`` (r, sett
     |> toFiveDec
 
   Assert.Equal<float>(r, price')
+
+
+[<Theory>]
+//           result    settl         maturity     rate      price   redemption  freq
+[<InlineData(0.0920, "2023-01-15", "2029-04-20", 0.07294,  91.0,   100.,       4.)>]
+let ``Test valid inputs for Yield.price function for UsPsa30_300`` (r, settl, mat, rate, pr, redemption, freq) =
+  let settl' = DateTime.Parse(settl)
+  let mat' = DateTime.Parse(mat)
+
+  let yield' = 
+    yieldFunc settl' mat' rate pr redemption freq Excel.FinancialFunctions.DayCountBasis.UsPsa30_360
+    |> toFourDec
+
+  Assert.Equal<float>(r, yield')
 
 
 [<Fact>]
